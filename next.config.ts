9f35661e116a56278f3327220831c9c1f84992eb
shishirchadha <shishirchadha@outlook.com>
2026-03-25 01:07:07 +0530

    remove basePath so app runs at root of subdomain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/official',
-  assetPrefix: '/official',
   turbopack: {
-    root: process.cwd(), // this tells Turbopack to use the current directory
+    root: process.cwd(),
   },
 };
 
